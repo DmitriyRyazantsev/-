d9f9415801ea31f821158755f08397267db0bb99
Dmitry <dmitry.ryazantsev@mail.ru>
2023-04-23 21:15:20 +0300

    mili

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        int x = 100;
+        int mile = 20;
+        int bonus = x / mile;
+        System.out.println("Ваши" + bonus + "Бонусных миль");
+    }
+}
